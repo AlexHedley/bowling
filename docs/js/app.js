9f7e2205714545ce6e4f78f80224de04bf213144
Alex Hedley <alex.hedley@me.com>
2022-10-21 17:53:35 +0100

    League Stats

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,11 +1,17 @@
 var myApp = angular.module("myApp", []);
 myApp.controller("myController", function ($scope, $http, $q, $filter) {
   $scope.scores = [];
-  $scope.leagueScores = [];
   $scope.maxScore = 0;
   $scope.minScore = 0;
   $scope.avgScore = 0.0; //=Round(SumScore/NumGames)
   $scope.count = 0;
+
+  $scope.leagueScores = [];
+  $scope.maxLeagueScore = 0;
+  $scope.minLeagueScore = 0;
+  $scope.avgLeagueScore = 0.0; //=Round(SumScore/NumGames)
+  $scope.maxLeagueSeriesScore = 0;
+  $scope.leagueCount = 0;
 
   $scope.init = function () {
     getData();
@@ -18,38 +24,71 @@ myApp.controller("myController", function ($scope, $http, $q, $filter) {
       $scope.scores = response.data.item.filter((s) => s.league === false);
       $scope.leagueScores = response.data.item.filter((s) => s.league === true);
 
-      var totalSum = Object.keys($scope.scores)
-        .map(function (k) {
-          return +$scope.scores[k].Score10;
-        })
-        .reduce(function (a, b) {
-          return a + b;
-        }, 0);
+      var totalSum = getTotal($scope.scores)
       var numGames = $scope.scores.length;
       var avgScore = totalSum / numGames;
       $scope.avgScore = avgScore;
+      $scope.minScore = getMin($scope.scores);
+      $scope.maxScore = getMax($scope.scores);
+      $scope.count = numGames;
 
-      var min = Math.min.apply(
-        Math,
-        $scope.scores.map(function (item) {
-          return item.Score10;
-        })
-      );
-      $scope.minScore = min;
-      var max = Math.max.apply(
-        Math,
-        $scope.scores.map(function (item) {
-          return item.Score10;
-        })
-      );
-      $scope.maxScore = max;
-
-      $scope.count = $scope.scores.length;
+      var totalLeagueSum = getTotal($scope.leagueScores)
+      var numLeagueGames = $scope.leagueScores.length;
+      var avgLeagueScore = totalLeagueSum / numLeagueGames;
+      $scope.avgLeagueScore = avgLeagueScore;
+      $scope.minLeagueScore = getMin($scope.leagueScores);
+      $scope.maxLeagueScore = getMax($scope.leagueScores);
+      $scope.maxLeagueSeriesScore = getMaxLeagueSeries($scope.leagueScores);
+      $scope.countLeague = numLeagueGames;
     });
   };
 
   $scope.init();
 });
+
+function getMax(scores) {
+    var max = Math.max.apply(
+        Math,
+        scores.map(function (item) {
+          return item.Score10;
+        })
+      );
+    return max;
+}
+
+function getMin(scores) {
+    var min = Math.min.apply(
+        Math,
+        scores.map(function (item) {
+          return item.Score10;
+        })
+      );
+    return min;
+}
+
+function getTotal(scores) {
+    var totalSum = Object.keys(scores)
+        .map(function (k) {
+          return +scores[k].Score10;
+        })
+        .reduce(function (a, b) {
+          return a + b;
+        }, 0);
+    
+    return totalSum;
+}
+
+function getMaxLeagueSeries(scores) {
+    var result = Object.values(scores.reduce((r, o) => {
+        r[o.date] = r[o.date] || {Date: o.date, Score10 : 0};
+        r[o.date].Score10 += +o.Score10;
+        return r;
+    },{}));
+    // console.log(result)
+
+    var max = Math.max(...result.map(o => o.Score10))
+    return max;
+}
 
 myApp.filter("roundTo", function (numberFilter) {
   return function (value, maxDecimals) {
