b5e1882448f00ccad2612e8f87fec38237715b74
Alex Hedley <alex.hedley@me.com>
2022-11-13 17:21:14 +0000

    Added HCP Scores to graph

--- docs/js/app.js
+++ docs/js/app.js
@@ -21,125 +21,165 @@ myApp.controller("myController", function ($scope, $http, $q, $filter) {
   getData = () => {
     var file = "data/scores.json";
 
-    $http.get(file)
-      .then(function (response) {
-        $scope.scores = response.data.item.filter((s) => s.league === false);
-        $scope.leagueScores = response.data.item.filter((s) => s.league === true);
+    $http.get(file).then(function (response) {
+      $scope.scores = response.data.item.filter((s) => s.league === false);
+      $scope.leagueScores = response.data.item.filter((s) => s.league === true);
 
-        var totalSum = getTotal($scope.scores)
-        var numGames = $scope.scores.length;
-        var avgScore = totalSum / numGames;
-        $scope.avgScore = avgScore;
-        $scope.minScore = getMin($scope.scores);
-        $scope.maxScore = getMax($scope.scores);
-        $scope.count = numGames;
+      var totalSum = getTotal($scope.scores);
+      var numGames = $scope.scores.length;
+      var avgScore = totalSum / numGames;
+      $scope.avgScore = avgScore;
+      $scope.minScore = getMin($scope.scores);
+      $scope.maxScore = getMax($scope.scores);
+      $scope.count = numGames;
 
-        var totalLeagueSum = getTotal($scope.leagueScores)
-        var numLeagueGames = $scope.leagueScores.length;
-        var avgLeagueScore = totalLeagueSum / numLeagueGames;
-        $scope.avgLeagueScore = avgLeagueScore;
-        $scope.minLeagueScore = getMin($scope.leagueScores);
-        $scope.maxLeagueScore = getMax($scope.leagueScores);
-        $scope.maxLeagueSeriesScore = getMaxLeagueSeries($scope.leagueScores);
-        $scope.avgMaxLeagueSeriesScore = $scope.maxLeagueSeriesScore / 3
-        $scope.countLeague = numLeagueGames;
+      var totalLeagueSum = getTotal($scope.leagueScores);
+      var numLeagueGames = $scope.leagueScores.length;
+      var avgLeagueScore = totalLeagueSum / numLeagueGames;
+      $scope.avgLeagueScore = avgLeagueScore;
+      $scope.minLeagueScore = getMin($scope.leagueScores);
+      $scope.maxLeagueScore = getMax($scope.leagueScores);
+      $scope.maxLeagueSeriesScore = getMaxLeagueSeries($scope.leagueScores);
+      $scope.avgMaxLeagueSeriesScore = $scope.maxLeagueSeriesScore / 3;
+      $scope.countLeague = numLeagueGames;
 
-        var keys = $scope.leagueScores.map(s => new Date(s.date).toLocaleDateString()); //.toLocaleString('en')) //.toLocaleFormat('%d %b %Y'));
-        var scores = $scope.leagueScores.map(s => s.Score10);
-        // Chart
-        generateChart(keys, scores, avgLeagueScore);
-      });
+      var keys = $scope.leagueScores.map((s) =>
+        new Date(s.date).toLocaleDateString()
+      ); //.toLocaleString('en')) //.toLocaleFormat('%d %b %Y'));
+      var scores = $scope.leagueScores.map((s) => s.Score10);
+
+      var scoresHCP = $scope.leagueScores.map(
+        (s) => parseInt(s.Score10) + parseInt(s.hcp)
+      );
+      console.log(scoresHCP);
+      var totalLeagueSumHCP = getTotalHCP($scope.leagueScores);
+      var avgLeagueScoreHCP = totalLeagueSumHCP / numLeagueGames;
+
+      // Chart
+      generateChart(keys, scores, avgLeagueScore, scoresHCP, avgLeagueScoreHCP);
+    });
   };
 
-  generateChart = (keys, values, avg) => {
+  generateChart = (keys, values, avg, valuesHCP, avgHCP) => {
     const options = {
       plugins: {
-          autocolors: false,
-          datalabels: {
-              color: '#36A2EB'
+        autocolors: false,
+        datalabels: {
+          color: "#36A2EB",
+        },
+        annotation: {
+          annotations: {
+            line1: {
+              type: "line",
+              yMin: avg,
+              yMax: avg,
+              borderColor: "rgb(0, 0, 0)",
+              borderWidth: 2,
+            },
+            line2: {
+              type: "line",
+              yMin: avgHCP,
+              yMax: avgHCP,
+              borderColor: "rgb(0, 0, 0)",
+              borderWidth: 2,
+            },
           },
-          annotation: {
-            annotations: {
-              line1: {
-                type: 'line',
-                yMin: avg,
-                yMax: avg,
-                borderColor: 'rgb(0, 0, 0)',
-                borderWidth: 2,
-              }
-            }
-          }
-      }
+        },
+      },
     };
 
     const config = {
-      type: 'line',
+      type: "line",
       data: {
-          labels: keys,
-          datasets: [
-            {
-              label: 'Scores',
-              data: values,
-              fill: false,
-              borderColor: 'red',
-              tension: 0.1,
-              datalabels: {
-                  color: '#FFCE56'
-              }
-            }
-          ]
-        },
-        options
+        labels: keys,
+        datasets: [
+          {
+            label: "Scores",
+            data: values,
+            fill: false,
+            borderColor: "red",
+            tension: 0.1,
+            datalabels: {
+              color: "#FFCE56",
+            },
+          },
+          {
+            label: "Scores (HCP)",
+            data: valuesHCP,
+            fill: false,
+            borderColor: "blue",
+            tension: 0.1,
+            datalabels: {
+              color: "#FFCE56",
+            },
+          },
+        ],
+      },
+      options,
     };
     let ctx = $("#myChart");
     let lineGraph = new Chart(ctx, config);
-  }
+  };
 
   $scope.init();
 });
 
 function getMax(scores) {
-    var max = Math.max.apply(
-        Math,
-        scores.map(function (item) {
-          return item.Score10;
-        })
-      );
-    return max;
+  var max = Math.max.apply(
+    Math,
+    scores.map(function (item) {
+      return item.Score10;
+    })
+  );
+  return max;
 }
 
 function getMin(scores) {
-    var min = Math.min.apply(
-        Math,
-        scores.map(function (item) {
-          return item.Score10;
-        })
-      );
-    return min;
+  var min = Math.min.apply(
+    Math,
+    scores.map(function (item) {
+      return item.Score10;
+    })
+  );
+  return min;
 }
 
 function getTotal(scores) {
-    var totalSum = Object.keys(scores)
-        .map(function (k) {
-          return +scores[k].Score10;
-        })
-        .reduce(function (a, b) {
-          return a + b;
-        }, 0);
-    
-    return totalSum;
+  var totalSum = Object.keys(scores)
+    .map(function (k) {
+      return +scores[k].Score10;
+    })
+    .reduce(function (a, b) {
+      return a + b;
+    }, 0);
+
+  return totalSum;
+}
+
+function getTotalHCP(scores) {
+  var totalSum = Object.keys(scores)
+    .map(function (k) {
+      return +(parseInt(scores[k].Score10) + parseInt(scores[k].hcp));
+    })
+    .reduce(function (a, b) {
+      return a + b;
+    }, 0);
+
+  return totalSum;
 }
 
 function getMaxLeagueSeries(scores) {
-    var result = Object.values(scores.reduce((r, o) => {
-        r[o.date] = r[o.date] || {Date: o.date, Score10 : 0};
-        r[o.date].Score10 += +o.Score10;
-        return r;
-    },{}));
-    // console.log(result)
+  var result = Object.values(
+    scores.reduce((r, o) => {
+      r[o.date] = r[o.date] || { Date: o.date, Score10: 0 };
+      r[o.date].Score10 += +o.Score10;
+      return r;
+    }, {})
+  );
+  // console.log(result)
 
-    var max = Math.max(...result.map(o => o.Score10))
-    return max;
+  var max = Math.max(...result.map((o) => o.Score10));
+  return max;
 }
 
 myApp.filter("roundTo", function (numberFilter) {
