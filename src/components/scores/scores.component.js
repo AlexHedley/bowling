cfc89bc9974979744cb547e97dfa1d2a66cc9876
Alex Hedley <alex.hedley@me.com>
2022-12-04 19:23:41 +0000

    Working on formatting scores

--- src/components/scores/scores.component.js
+++ src/components/scores/scores.component.js
@@ -1,5 +1,23 @@
 function ScoresController() {
   var ctrl = this;
+
+  function getClass(score) {
+    return {
+      strike: score === "X",
+      hardSpare: score.length > 1,
+    };
+  }
+
+  function trim(score) {
+    if (score.length > 1) {
+      return score.substring(0, 1);
+    } else {
+      return score;
+    }
+  }
+
+  this.getClass = getClass;
+  this.trim = trim;
 }
 angular.module("myApp").component("scores", {
   templateUrl: "components/scores/scores.html",
