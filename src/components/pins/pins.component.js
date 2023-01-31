bb4c75f205ff60a28d6ba913b8b364c2c480c1f4
Alex Hedley <alex.hedley@me.com>
2023-01-31 18:29:13 +0000

    pins

--- src/components/pins/pins.component.js
+++ src/components/pins/pins.component.js
@@ -1,5 +1,17 @@
 function PinsController() {
   var ctrl = this;
+
+  function symbol(frame) {
+    if (frame === "1") {
+      return "◯";
+    } else if (frame === "2") {
+      return "⦿";
+    } else {
+      return "⬤";
+    }
+  }
+
+  this.symbol = symbol;
 }
 angular.module("myApp").component("pins", {
   templateUrl: "components/pins/pins.html",
